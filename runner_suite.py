40b5aae9ea7a69c4c55407be651fb581986ca303
jseom0630 <jseom0630@gmail.com>
2023-04-18 17:39:42 +0900

    del report

--- runner_suite.py
+++ runner_suite.py
@@ -5,9 +5,10 @@ from test.testsuite.test_suite_1 import TestSuite1
 if __name__ == '__main__':
         # HTML Test Runner 설정
     runner = HtmlTestRunner.HTMLTestRunner(
-        output='reports',
+        output='../test_result/',
         report_name='testsuite_report',
         report_title='All Tests Results'
+      
         # combine_reports=True
     )
     suite = unittest.TestSuite()
